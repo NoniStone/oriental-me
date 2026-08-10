d532f51a6c8dc6cc81e86bc03a62c192a6eeda72
Stoney <vHackv119@gmail.com>
2026-08-10 20:40:39 +0800

    Fix home runtime state import

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { ArrowRight, Compass, Sparkles } from "lucide-react";
 import { Button } from "@/components/ui/button";
