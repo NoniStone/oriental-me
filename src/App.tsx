56f8914fe872a65fbb2d3e4edbbf64322097a92d
Stoney <vHackv119@gmail.com>
2026-08-10 18:03:38 +0800

    feat: embed challenge community and studio review

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,6 @@ import Discover from "./pages/Discover";
 import Plan from "./pages/Plan";
 import Settings from "./pages/Settings";
 import ProfileIntake from "./pages/ProfileIntake";
-import Social from "./pages/Social";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -58,7 +57,6 @@ const App = () => (
               element={guarded(<ChallengeDetail />)}
             />
             <Route path="/discover" element={guarded(<Discover />)} />
-            <Route path="/social" element={guarded(<Social />)} />
             <Route path="/settings" element={guarded(<Settings />)} />
             <Route
               path="/admin"
